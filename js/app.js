76636b5ebdb1371a4f42a6e23bb5c88db7db54b2
nggls <n.larar@alumnos.duoc.cl>
2020-01-11 22:40:00 -0300

    pagina registrar y validaciones

--- js/app.js
+++ js/app.js
@@ -23,3 +23,67 @@ $("#formularioIndex").validate({
         }
     }
 })
+$("#formularioRegistrar").validate({
+    errorClass: 'is-invalid',
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        contrasena: {
+            required: true,
+            minlength: 8,
+            maxlength: 16
+        },
+        nombre: {
+            required: true,
+
+        },
+        apellido: {
+            required: true,
+        },
+        ciudad: {
+            required: true,
+            minlength: 8,
+            maxlength: 16
+        },
+        telefono: {
+            required: true,
+            minlength: 9
+        }
+
+    },
+    messages: {
+        email: {
+            required: "Este campo no puede estar vacio",
+            email: "Debe ser en formato de email"
+        },
+        contraseña: {
+            required: "Este campo no puede estar vacio",
+            minlength: 8,
+            maxlength: 16
+        },
+        nombre: {
+            required: "Este campo no puede estar vacio",
+        },
+        apellido: {
+            required: "Este campo no puede estar vacio"
+
+        },
+        ciudad: {
+            required: "Este campo no puede estar vacio"
+
+        },
+        telefono: {
+            required: "Este campo no puede estar vacio",
+            minlength: "Debe tener un minimo de 9 caracteres"
+        }
+
+    }
+})
+$(".correcto").click(function () {
+    swal("Perfecto!");
+});
+$(".incorrecto").click(function () {
+    swal("Mal!");
+});
