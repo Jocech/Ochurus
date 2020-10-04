a8fbc99d8689ed4488120b0c87a063fe99ec502e
Jocech <josehurtubia@hotmail.com>
2020-10-04 17:38:13 -0300

    Actualizacion a routers para que funcione django

--- Ochurus/usuario/routers.py
+++ Ochurus/usuario/routers.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 
 #los routers proveen una manera fácil de determinar la configuracion URL
 router = routers.DefaultRouter()
-router.register(r'usuarios',UserViewSet,base_name='user')
-router.register(r'campeones',CampeonViewSet,base_name='campeones')
+router.register(r'usuarios',UserViewSet,basename='user')
+router.register(r'campeones',CampeonViewSet,basename='campeones')
